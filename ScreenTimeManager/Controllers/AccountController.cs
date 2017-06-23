928e13e56703f8255eae3e07c1c4b0f204612396
jnims <jmnims@gmail.com>
2017-06-22 20:28:32 -0400

    Messed some things up, then fixed them

--- ScreenTimeManager/Controllers/AccountController.cs
+++ ScreenTimeManager/Controllers/AccountController.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
+﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using ScreenTimeManager.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 
 namespace ScreenTimeManager.Controllers
 {
-    [Authorize]
+	[Authorize]
     public class AccountController : Controller
     {
         private ApplicationSignInManager _signInManager;
