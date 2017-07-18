c55630e4f5c62bdcd9f4588aa1d658a32e84efb7
jnims <jmnims@gmail.com>
2017-07-18 10:51:59 -0400

    Added proper initializer

--- ScreenTimeManager/Global.asax.cs
+++ ScreenTimeManager/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ScreenTimeManager.DataModel.DataContexts;
 
 namespace ScreenTimeManager
 {
@@ -16,6 +18,13 @@ namespace ScreenTimeManager
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+			Database.SetInitializer(new DataModel.Initializers.ScreenTimeManagerInitializer());
+
+	        using (ScreenTimeManagerContext ctx = new ScreenTimeManagerContext())
+	        {
+		        ctx.Database.Initialize(true);
+	        }
         }
     }
 }
