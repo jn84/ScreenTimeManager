da129d56cb5d8542ac6782de501481f413252bd0
jnims <jmnims@gmail.com>
2017-08-03 19:09:30 -0400

    Changed model

--- ScreenTimeManager.Models/TotalScreenTimeChanged.cs
+++ ScreenTimeManager.Models/TotalScreenTimeChanged.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
@@ -11,12 +12,18 @@ namespace ScreenTimeManager.Models
 	{
 		public int Id { get; set; }
 
+		[Required]
 		public long SecondsAdded { get; set; }
 
+		[Required]
 		public DateTime RecordAddedDateTime { get; set; }
 
 		// The rule used for this entry into the history
 		// At least one, but not more than one
+		[Required]
 		public int RuleUsedId { get; set; }
+
+
+		public string SubmissionNote { get; set; }
 	}
 }
