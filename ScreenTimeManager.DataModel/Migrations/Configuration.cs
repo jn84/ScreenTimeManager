b7936adbeba094755da2081d04ac77dc58f97455
jnims <jmnims@gmail.com>
2017-08-03 10:54:44 -0400

    Of course it wouldn't be so easy.

--- ScreenTimeManager.DataModel/Migrations/Configuration.cs
+++ ScreenTimeManager.DataModel/Migrations/Configuration.cs
@@ -9,11 +9,11 @@ namespace ScreenTimeManager.DataModel.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<ScreenTimeManager.DataModel.DataContexts.ScreenTimeManagerContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<DataContexts.ScreenTimeManagerContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(ScreenTimeManager.DataModel.DataContexts.ScreenTimeManagerContext context)
